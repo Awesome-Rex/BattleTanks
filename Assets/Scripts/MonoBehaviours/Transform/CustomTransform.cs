56ad997cc9295ddd942edbf8b17baa7ef73109b4
Awesome-Rex <john.esqqq@gmail.com>
2020-06-18 16:09:21 -0400

    Probably finished base classes fo CustomTransform

--- Assets/Scripts/MonoBehaviours/Transform/CustomTransform.cs
+++ Assets/Scripts/MonoBehaviours/Transform/CustomTransform.cs
@@ -4,47 +4,28 @@ using UnityEngine;
 
 public abstract class CustomTransform<T> : MonoBehaviour
 {
+    //properties
     public Space space = Space.Self;
-
-    public bool follow = false;
-    public Transition transition;
-
-    //global
-    public T value;
-
-    //local
+    
+    protected T value;
     public Transform parent;
-
-    public Link link = Link.Offset;
-
-    public T localOffset;
-    public T globalOffset;
-
-
-    //previous
+    
     private T previous;
 
-    [ContextMenu("Apply to target")]
-    public abstract void ApplyToTarget();
-
-    public abstract void SetTarget();
-
-    public abstract Vector3 GetTarget();
-
+    //methods
+    public abstract T GetTarget();
 
     public abstract void SetPrevious();
 
-    private void Awake()
+    protected virtual void Awake()
     {
         SetPrevious();
 
         _ETERNAL.r.lateRecorder.callback += SetPrevious;
-        _ETERNAL.r.earlyRecorder.callback += SetTarget;
     }
 
-    private void OnDestroy()
+    protected virtual void OnDestroy()
     {
         _ETERNAL.r.lateRecorder.callback -= SetPrevious;
-        _ETERNAL.r.earlyRecorder.callback -= SetTarget;
     }
 }
---
+++ Assets/Scripts/MonoBehaviours/Transform/CustomTransformLinks.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CustomTransformLinks<T> : CustomTransform<T>
+{
+    public bool follow = false;
+    public Transition transition;
+
+
+    public Link link = Link.Offset;
+    
+
+    public T globalOffset;  //local
+
+    [ContextMenu("Apply to target")]
+    public abstract void ApplyToTarget();
+
+    public abstract void SetTarget();
+
+
+    protected override void Awake ()
+    {
+        base.Awake();
+        _ETERNAL.r.earlyRecorder.callback += SetTarget;
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        _ETERNAL.r.earlyRecorder.callback -= SetTarget;
+    }
+}
