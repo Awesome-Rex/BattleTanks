2dd1f9db9c2b1dd067eb69c546d0384395ef6453
Awesome-Rex <john.esqqq@gmail.com>
2020-07-02 09:33:47 -0400

    Re-arranged everything (Space puppy, NotCompiled)

--- Assets/Scripts/Types/Structures/AxisApplied.cs
+++ Assets/Scripts/Types/Structures/AxisApplied.cs
@@ -81,6 +81,67 @@ public struct AxisApplied
                 //END
             });
         }
+	   
+	   /*Rect indentedPosition = EditorGUI.IndentedRect(position);
+
+            EditorGUI.BeginProperty(indentedPosition, label, property);
+
+
+            Color backgroundColorOG = GUI.backgroundColor;
+            int indentLevelOG = EditorGUI.indentLevel;
+
+            Rect newPosition = indentedPosition;
+            newPosition.width /= 4;
+            newPosition.x = indentedPosition.x;
+
+
+            GUI.backgroundColor = new Color(219f / 255f, 62f / 255f, 29f / 255f);
+            //if (((AxisApplied)(fieldInfo.GetValue(property.serializedObject.targetObject) as AxisApplied?)).axis == Axis.X)
+            //{
+                if (GUI.Button(newPosition, "X", EditorStyles.miniButton))
+                {
+
+                }
+            //}
+
+            newPosition.x += (indentedPosition.width / 4);
+            //if (((AxisApplied)(fieldInfo.GetValue(property.serializedObject.targetObject) as AxisApplied?)).axis == Axis.Y)
+            //{
+                GUI.backgroundColor = new Color(154 / 255f, 243 / 255f, 72f / 255f);
+                if (GUI.Button(newPosition, "Y", EditorStyles.miniButton))
+                {
+
+                }
+            //}
+
+            newPosition.x += (indentedPosition.width / 4);
+            //if (((AxisApplied)(fieldInfo.GetValue(property.serializedObject.targetObject) as AxisApplied?)).axis == Axis.Z)
+            //{
+                GUI.backgroundColor = new Color(58f / 255f, 122f / 255f, 237f / 255f);
+                if (GUI.Button(newPosition, "Z", EditorStyles.miniButton))
+                {
+                    //fieldInfo.SetValue(property.serializedObject.targetObject, )
+                }
+            //}
+
+            //var field = property.serializedObject.targetObject.GetType().GetField(property.propertyPath);
+            //Debug.Log(field);
+            //if (field != null)
+            //{
+            //    var value = field.GetValue(property.serializedObject.targetObject);
+            //    Debug.Log(((AxisApplied)(value as AxisApplied?)).axis);
+            //}
+
+
+
+            GUI.backgroundColor = backgroundColorOG;
+
+            EditorGUI.indentLevel = 0;
+            newPosition.x += (indentedPosition.width / 4);
+            EditorGUI.PropertyField(newPosition, property.FindPropertyRelative("units"), GUIContent.none);
+            EditorGUI.indentLevel = indentLevelOG;
+
+            EditorGUI.EndProperty();*/
     }
 #endif
 }
