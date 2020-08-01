41febfa35e4c222bfd171b5217c0c0a92fca9166
Awesome-Rex <john.esqqq@gmail.com>
2020-08-01 17:29:12 -0400

    Experimenting with shaders and trying to create an outline

---
+++ Assets/Scripts/MonoBehaviours/HideBehind.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideBehind : MonoBehaviour
+{
+    public List<MeshRenderer> toHideBehind;
+    
+    void Update()
+    {
+        //hide behind
+    }
+}
