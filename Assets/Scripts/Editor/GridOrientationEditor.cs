384b3393918f94ef67f5dd2218ed1071241ffa77
Awesome-Rex <john.esqqq@gmail.com>
2021-03-31 18:49:57 -0400

    A WHOLE LOT OF STUFF (GRID LOGIC is WORKING!!!)
    Grid scripts have spacing and Vector3 scale removed
    Grid orientation methods are all WORKING
    Implemented grid subdivisions
    Visual Grid Gizmos are fully WORKING! (still need to add transform handles)

---
+++ Assets/Scripts/Editor/GridOrientationEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace REXTools.Tiling
+{
+    [CustomEditor(typeof(GridOrientation))]
+    public class GridOrientationEditor : Editor
+    {
+        public void OnSceneGUI ()
+        {
+            //DRAW HANDLES TO OFFSET PLANE GRIDS!
+        }
+    }
+}
---
+++ Assets/Scripts/MonoBehaviours/Tiling/GridPlaneOrientation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using REXTools.TransformTools;
+
+namespace REXTools.Tiling
+{
+    [System.Serializable]
+    public class GridPlaneOrientation
+    {
+        public bool enabled = true;
+
+
+        //Gizmos orientation
+        public UnityEngine.Vector2Int size;
+
+        public UnityEngine.Vector3Int offset = UnityEngine.Vector3Int.zero;
+        public float normalOffset = 0f;
+
+        public int subdivisions = 1;
+
+        public bool planeOnEdge
+        {
+            get
+            {
+                return normalOffset % 1f == 0.5f || normalOffset % 1f == -0.5f;
+            }
+        }
+
+        public GridPlaneOrientation(
+            UnityEngine.Vector2Int size = default,
+            UnityEngine.Vector3Int offset = default,
+            float normalOffset = 0f,
+            int subdivisions = 1
+        ) {
+            this.enabled = true;
+
+            this.size = size;
+            this.offset = offset;
+            this.normalOffset = normalOffset;
+            this.subdivisions = subdivisions;
+        }
+    }
+}
